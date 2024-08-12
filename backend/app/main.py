7775e7e1430220e509fee26b23118abc1653c452
MountComb <69704258+MountComb@users.noreply.github.com>
2024-08-12 23:21:42 +0800

    backend settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,24 @@
+from pathlib import Path
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings
 
+class DaysAPISettings(BaseSettings):
+    data_path: Path = "./"
+
+
+settings = DaysAPISettings()
 app = FastAPI()
+
+
+if not settings.data_path.exists():
+    settings.data_path.mkdir(parents=True)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/settings")
+async def get_settings():
+    return settings.model_dump()
